0917c86b59d13c43cf20bcc9ab1f3f8c79ebf702
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-31 06:35:08 +0430

    addSchoolToTeacher and addCourseToTeacher methods added

--- src/com/school/TeacherRepository.java
+++ src/com/school/TeacherRepository.java
@@ -1,4 +1,26 @@
 package com.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TeacherRepository {
+
+    List<Teacher> teachers = new ArrayList<>();
+
+    public Teacher addSchoolToTeacher (Teacher teacher, School school){
+        teacher.getSchool().add(school);
+        return teacher;
+    }
+
+    public Teacher addCourseToTeacher (Teacher teacher, Course course){
+        teacher.getCourse().add(course);
+        return teacher;
+    }
+
+
+
+
+
+
 }
