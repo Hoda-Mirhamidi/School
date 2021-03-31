2363a61272b500df92c725e4a3e0b528be1230ea
Hoda-Mirhamidi <hodamirhamidi@gmail.com>
2021-03-31 06:36:34 +0430

    aboveAverageSalary method added

--- src/com/school/TeacherRepository.java
+++ src/com/school/TeacherRepository.java
@@ -1,7 +1,11 @@
 package com.school;
 
+import com.school.enums.TeacherType;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 
 public class TeacherRepository {
@@ -16,6 +20,15 @@ public class TeacherRepository {
     public Teacher addCourseToTeacher (Teacher teacher, Course course){
         teacher.getCourse().add(course);
         return teacher;
+    }
+
+    public List<Teacher> aboveAverageSalary (){
+        List<Teacher> filteredTeacherList = new ArrayList<>();
+        OptionalDouble average = teachers.stream().filter(teacher -> teacher.getType().equals(TeacherType.FULL_TIME)).mapToDouble(Teacher::calculateSalary).average();
+        if(average.isPresent()){
+            filteredTeacherList = teachers.stream().filter(teacher -> teacher.calculateSalary() > average.getAsDouble()).collect(Collectors.toList());
+        }
+        return filteredTeacherList;
     }
 
 
